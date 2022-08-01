460e0e73a396db99815589cdc2dc20899a6ef072
msparth89@gmail.com-at-707163464977 <msparth89@gmail.com>
2022-08-01 16:22:59 +0530

    added javascript compiling

--- index.js
+++ index.js
@@ -2,35 +2,47 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path')
 const formidable = require('formidable');
+var childProcess = require('child_process');
 
 const app = express();
 
-app.post('/api/upload', (req, res, next) => {
-    console.log("api hittt")
-	
-	const form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files){
+app.use(express.text())
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	next();
 
-        //console.log(req);
-        //console.log(files);
-       // console.log(files.file.filepath+"           "+files.file.originalFilename);
-        console.log(req);
+})
 
-		var oldPath = files.file.filepath;
-		var newPath = path.join(__dirname, 'uploads')
-        + '/'+files.file.originalFilename;
-		var rawData = fs.readFileSync(oldPath)
-	
-		fs.writeFile(newPath, rawData, function(err){
-			if(err) console.log(err)
-			return res.send("Successfully uploaded")
-		})
+app.post('/api/javascript', (req, res) => {
+	var data = JSON.parse(req.body).code;
+	fs.writeFile("code.txt", data, (err) => {
+		if (err)
+		{
+			console.log(err);
+			res.end(JSON.stringify({ "body":"Error at server, please report to moderator"}));
+		}
+		else {
+				childProcess.execFile('node', ["code.txt"], function (err, stdout, stderr) {
+					if (err) {
+						// handle err
+						res.end(JSON.stringify({ "body": err.toString() }));
+					}
+					else if (stdout) {
+						res.end(JSON.stringify({ "body": stdout.toString() }));
+					}
+					else  {						
+							res.end(JSON.stringify({ "body": stderr.toString() }));
+					}
+				});
+		}
+		//fs.unlinkSync("code.txt");
+	});
+
 })
 
 
-});
-
-app.listen(3005, function(err){
-	if(err) console.log(err)
+app.listen(3005, function (err) {
+	//if(err) console.log(err)
 	console.log('Server listening on Port 300s');
 });
